1efc96639cc2e4763b03b1fa1085c0307aaadcb8
Deziree' Teague <dez.teague@gmail.com>
2019-03-11 11:44:20 -0700

    create task method in service

--- Maintain.NET/Maintain.NET/Models/Services/TaskManagementService.cs
+++ Maintain.NET/Maintain.NET/Models/Services/TaskManagementService.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Maintain.NET.Data;
+using Maintain.NET.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Maintain.NET.Models.Services
 {
-    public class TaskManagementService
+    public class TaskManagementService : ITaskManager
     {
+        private MaintainDbContext _context { get; }
+
+        public TaskManagementService(MaintainDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateTask (MaintenanceTask maintenanceTask)
+        {
+            _context.MaintenanceTasks.Add(maintenanceTask);
+            await _context.SaveChangesAsync();
+        }
     }
 }
