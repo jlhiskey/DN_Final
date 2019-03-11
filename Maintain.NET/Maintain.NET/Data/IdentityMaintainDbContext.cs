b2393f3e6d4f303080d2010e9fd2ccc6601ed51d
Jason Few <omence11@gmail.com>
2019-03-11 09:54:29 -0700

    Identity db created

--- Maintain.NET/Maintain.NET/Data/IdentityMaintainDbContext.cs
+++ Maintain.NET/Maintain.NET/Data/IdentityMaintainDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Maintain.NET.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,21 +8,14 @@ using System.Threading.Tasks;
 
 namespace Maintain.NET.Data
 {
-    public class IdentityMaintainDbContext : DbContext
+    public class IdentityMaintainDbContext : IdentityDbContext<ApplicationUser>
     {
         public IdentityMaintainDbContext(DbContextOptions<IdentityMaintainDbContext> options) : base(options)
         {
 
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            //TODO: Add Seeds here
-
-
-        }
-
-        //TODO: Add table references here.
+     
 
     }
 }
