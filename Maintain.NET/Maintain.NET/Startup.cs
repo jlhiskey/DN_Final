7ed327dd7ebc974404e55a1ab2bca378e76ef129
Jason Hiskey <jasonhi@crazyredhead.com>
2019-03-11 08:40:47 -0700

    Added user secrets and dbContext

--- Maintain.NET/Maintain.NET/Startup.cs
+++ Maintain.NET/Maintain.NET/Startup.cs
@@ -2,20 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Maintain.NET.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Maintain.NET
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            //Add Identity Here
+
+
+            //Add Authorization Here
+
+
+            //Add Dependency Injection Here
+
+            // Switches between connection strings.
+            bool usingProduction = true;
+
+            //*********************DEFAULT CONNECTION STRINGS******************************************************************
+            if (!usingProduction)
+            {
+                // Add default connection strings here.
+                services.AddDbContext<MaintainDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
+                services.AddDbContext<IdentityMaintainDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:IdentityDefaultConnection"]));
+
+            }
+
+            //*********************PRODUCTION CONNECTION STRINGS***************************************************************
+            if (usingProduction)
+            {
+                // Add production connection strings here.
+                services.AddDbContext<MaintainDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:ProductionConnection"]));
+                services.AddDbContext<IdentityMaintainDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:IdentityProductionConnection"]));
+
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,11 +75,7 @@ namespace Maintain.NET
                 name: "default",
                 template: "{controller=Home}/{action=Index}/{id?}");
             });
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            
         }
     }
 }
