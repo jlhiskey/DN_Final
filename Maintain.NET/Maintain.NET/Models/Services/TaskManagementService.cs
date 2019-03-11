70a2949dd66bd0ab799af874a17b380d1c2ee2e9
Deziree' Teague <dez.teague@gmail.com>
2019-03-11 11:48:00 -0700

    get task method in service

--- Maintain.NET/Maintain.NET/Models/Services/TaskManagementService.cs
+++ Maintain.NET/Maintain.NET/Models/Services/TaskManagementService.cs
@@ -1,5 +1,6 @@
 ﻿using Maintain.NET.Data;
 using Maintain.NET.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,5 +22,27 @@ namespace Maintain.NET.Models.Services
             _context.MaintenanceTasks.Add(maintenanceTask);
             await _context.SaveChangesAsync();
         }
+
+        public async Task<MaintenanceTask> GetTask(int id)
+        {
+            return await _context.MaintenanceTasks.FirstOrDefaultAsync(tsk => tsk.ID == id);
+        }
+
+     
+        Task<IEnumerable<MaintenanceTask>> GetAllTasks();
+
+        //read a user's task
+        Task<UserMaintenanceTask> GetUserMaintenancTask(int id);
+
+        //read all of a user's tasks
+        Task<UserMaintenanceTask> GetAllUserMaintenanceTasks(int id);
+
+        //update/edit
+        Task UpdateTask();
+
+        //delete
+        Task DeleteTask(int id);
+
+        bool TaskExists(int id);
     }
 }
