20204a75151f216c1d23010cd3e436a0fe33215b
Jason Hiskey <jasonhi@crazyredhead.com>
2019-03-11 09:54:40 -0700

    MaintenanceTask Model Complete

---
+++ Maintain.NET/Maintain.NET/Models/MaintenanceTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Maintain.NET.Models
+{
+    public class MaintenanceTask
+    {
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Name of MaintenanceTask
+        /// </summary>
+        [Required(ErrorMessage = "Please provide a name for your task")]
+        [Display(Name = "Task Name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Recommended maintenance interval of MaintenanceTask
+        /// </summary>
+        [Display(Name = "Recommended Interval")]
+        public int RecommendedInterval { get; set; }
+
+        // Navigation Properties
+        /// <summary>
+        /// Collection of UserMaintenanceTasks based off of ID
+        /// </summary>
+        public ICollection<UserMaintenanceTask> UserMaintenanceTasks { get; set; }
+
+        /// <summary>
+        /// Creates a new MaintenanceTask with input name.
+        /// </summary>
+        /// <param name="name">string input that will set the Name of the new MaintenanceTask</param>
+        public MaintenanceTask(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
