f2f2e6b3370eddb553500126fe3e159f04e08d31
Deziree' Teague <dez.teague@gmail.com>
2019-03-11 11:35:05 -0700

    created method signatures in interface

--- Maintain.NET/Maintain.NET/Controllers/TaskController.cs
+++ Maintain.NET/Maintain.NET/Controllers/TaskController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Maintain.NET.Data;
+using Maintain.Net.Models;
+using Maintain.NET.Models.Interfaces;
 
 namespace Maintain.NET.Controllers
 {
--- Maintain.NET/Maintain.NET/Models/Interfaces/ITaskManager.cs
+++ Maintain.NET/Maintain.NET/Models/Interfaces/ITaskManager.cs
@@ -7,5 +7,27 @@ namespace Maintain.NET.Models.Interfaces
 {
     interface ITaskManager
     {
+        //create 
+        Task CreateTask();
+
+        //read maintenance task
+        Task<MaintenanceTask> GetTask(int id);
+
+        //read all maintenance tasks
+        Task<IEnumerable<MaintenanceTask>> GetAllTasks();
+
+        //read a user's task
+        Task<UserMaintenanceTask> GetUserMaintenancTask(int id);
+
+        //read all of a user's tasks
+        Task<UserMaintenanceTask> GetAllUserMaintenanceTasks(int id);
+
+        //update/edit
+        Task UpdateTask();
+
+        //delete
+        Task DeleteTask(int id);
+
+        bool TaskExists(int id);
     }
 }
